3a126f27b9116f2351b82214209a72fe8c88f410
xzy52583752 <xzy52583752@outlook.com>
2019-08-21 15:51:33 +0800

    模板

---
+++ src/main/scala/com/utils/RptUtils.scala
@@ -0,0 +1,22 @@
+package com.utils
+
+object RptUtils {
+
+  // 此方法处理请求数
+//  def request(requestmode:Int,processnode:Int):List[Double]={
+//
+//  }
+//
+//  // 此方法处理展示点击数
+//
+//  def click(requestmode:Int,iseffective:Int):List[Double]={
+//
+//  }
+//  // 此方法处理竞价操作
+//
+//  def Ad(iseffective:Int,isbilling:Int,isbid:Int,iswin:Int,
+//         adorderid:Int,WinPrice:Double,adpayment:Double):List[Double]={
+//
+//  }
+
+}
